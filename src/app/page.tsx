74f65ce80c565c5786986ca1dfc5be55b9e75f6b
Roshan A <roshananand2005@gmail.com>
2026-01-23 14:50:13 +0530

    chore : hide scrollbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ const page = () => {
   return (
     <main className="w-[95%] max-w-200 h-screen mx-auto p-2 flex flex-col md:flex-row ">
       <SideBar />
-      <section className="flex-1 h-full overflow-y-auto p-4">
+      <section className="flex-1 h-full overflow-y-auto p-4 hide-scrollbar">
         <Projects />
         <Tech />
         <Hero />
